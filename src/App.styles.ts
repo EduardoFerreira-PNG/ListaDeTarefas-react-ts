8dd02813ef3459afc58fe9bf90a268cc3e794795
Eduardo <eduardonaves02@outlook.com>
2022-12-03 18:51:52 -0300

    finalização projeto tarefas pessoais

---
+++ src/App.styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #17181F;
+    color: #797a81;
+    min-height: 100vh;
+    font-family: Arial, 'sans-serif';
+`;
+
+export const Area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 10px;
+    
+`;
+
+export const Header = styled.h1`
+    padding: 0;
+    margin: 0;
+    text-align: center;
+    border-bottom: 1px solid #444;
+    padding-bottom: 20px;
+    color: #fff;
+`;
+
